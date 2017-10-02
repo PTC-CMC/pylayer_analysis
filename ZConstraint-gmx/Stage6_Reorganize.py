af9b33610ea70c2bb7e1266d027ac431869e3b25
ahy3nz <alexander.h.yang@vanderbilt.edu>
2017-10-02 11:00:59 -0500

    Record negative forces from the pulling

--- ZConstraint-gmx/Stage6_Reorganize.py
+++ ZConstraint-gmx/Stage6_Reorganize.py
@@ -13,7 +13,8 @@ from multiprocessing import Pool
 
 def _split_single_file(current_dir, sweep, i, j, all_forces):
     force_index = (j * N_sims) + i
-    np.savetxt(os.path.join(current_dir, "sweep{}/forceout{}.dat".format(sweep, force_index)), np.column_stack((all_forces[:,0], all_forces[:,j+1])))
+    data = [-1*force_val for force_val in all_forces[i,j+1]]
+    np.savetxt(os.path.join(current_dir, "sweep{}/forceout{}.dat".format(sweep, force_index)), np.column_stack((all_forces[:,0], data)))
     return 1
 
 
