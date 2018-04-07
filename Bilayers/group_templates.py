2f50e4d34bde4bfdc23eb74038c9e7903efada97
ahy3nz <alexander.h.yang@vanderbilt.edu>
2018-04-07 18:07:47 -0500

    FIx the reversed head/tail naming

--- Bilayers/group_templates.py
+++ Bilayers/group_templates.py
@@ -32,67 +32,67 @@ g53a6_groups['ISIS']['tail_2'] = [21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,3
 ### FFA groups ###
 ##################
 g53a6_groups['acd12'] = OrderedDict()
-g53a6_groups['acd12']['head'] = [0,1,2,3,4,5,6,7,8,9,10]
-g53a6_groups['acd12']['tail'] = [11,12,13,14]
+g53a6_groups['acd12']['tail'] = [0,1,2,3,4,5,6,7,8,9,10]
+g53a6_groups['acd12']['head'] = [11,12,13,14]
 
 g53a6_groups['acd14'] = OrderedDict()
-g53a6_groups['acd14']['head'] = [0,1,2,3,4,5,6,7,8,9,10,11,12]
-g53a6_groups['acd14']['tail'] = [13,14,15,16]
+g53a6_groups['acd14']['tail'] = [0,1,2,3,4,5,6,7,8,9,10,11,12]
+g53a6_groups['acd14']['head'] = [13,14,15,16]
 
 g53a6_groups['acd16'] = OrderedDict()
-g53a6_groups['acd16']['head'] = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
-g53a6_groups['acd16']['tail'] = [15,16,17,18]
+g53a6_groups['acd16']['tail'] = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+g53a6_groups['acd16']['head'] = [15,16,17,18]
 
 g53a6_groups['acd18'] = OrderedDict()
-g53a6_groups['acd18']['head'] = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
-g53a6_groups['acd18']['tail'] = [17,18,19,20]
+g53a6_groups['acd18']['tail'] = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+g53a6_groups['acd18']['head'] = [17,18,19,20]
 
 g53a6_groups['acd20'] = OrderedDict()
-g53a6_groups['acd20']['head'] = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
-g53a6_groups['acd20']['tail'] = [19,20,21,22]
+g53a6_groups['acd20']['tail'] = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+g53a6_groups['acd20']['head'] = [19,20,21,22]
 
 g53a6_groups['acd22'] = OrderedDict()
-g53a6_groups['acd22']['head'] = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-g53a6_groups['acd22']['tail'] = [21,22,23,24]
+g53a6_groups['acd22']['tail'] = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+g53a6_groups['acd22']['head'] = [21,22,23,24]
 
 g53a6_groups['acd24'] = OrderedDict()
-g53a6_groups['acd24']['head'] =[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]
-g53a6_groups['acd24']['tail'] = [23,24,25,26]
+g53a6_groups['acd24']['tail'] =[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]
+g53a6_groups['acd24']['head'] = [23,24,25,26]
 
 #################
 ### OH groups ###
 #################
 g53a6_groups['alc12'] = OrderedDict()
-g53a6_groups['alc12']['head'] = [0,1,2,3,4,5,6,7,8,9,10,11]
-g53a6_groups['alc12']['tail'] = [12,13]
+g53a6_groups['alc12']['tail'] = [0,1,2,3,4,5,6,7,8,9,10,11]
+g53a6_groups['alc12']['head'] = [12,13]
 
 g53a6_groups['alc14'] = OrderedDict()
-g53a6_groups['alc14']['head'] = [0,1,2,3,4,5,6,7,8,9,10,11,12,13]
-g53a6_groups['alc14']['tail'] = [14,15]
+g53a6_groups['alc14']['tail'] = [0,1,2,3,4,5,6,7,8,9,10,11,12,13]
+g53a6_groups['alc14']['head'] = [14,15]
 
 g53a6_groups['alc16'] = OrderedDict()
-g53a6_groups['alc16']['head'] = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-g53a6_groups['alc16']['tail'] = [16,17]
+g53a6_groups['alc16']['tail'] = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+g53a6_groups['alc16']['head'] = [16,17]
 
 g53a6_groups['alc18'] = OrderedDict()
-g53a6_groups['alc18']['head'] = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
-g53a6_groups['alc18']['tail'] = [18,19]
+g53a6_groups['alc18']['tail'] = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
+g53a6_groups['alc18']['head'] = [18,19]
 
 g53a6_groups['alc20'] = OrderedDict()
-g53a6_groups['alc20']['head'] = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
-g53a6_groups['alc20']['tail'] = [20,21]
+g53a6_groups['alc20']['tail'] = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+g53a6_groups['alc20']['head'] = [20,21]
 
 g53a6_groups['alc22'] = OrderedDict()
-g53a6_groups['alc22']['head'] = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]
-g53a6_groups['alc22']['tail'] = [22,23]
+g53a6_groups['alc22']['tail'] = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]
+g53a6_groups['alc22']['head'] = [22,23]
 
 g53a6_groups['alc24'] = OrderedDict()
-g53a6_groups['alc24']['head'] = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
-g53a6_groups['alc24']['tail'] = [24,25]
+g53a6_groups['alc24']['tail'] = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
+g53a6_groups['alc24']['head'] = [24,25]
 
  
 
-def g53a6_groups():
+def gromos53a6_groups():
     return g53a6_groups
 
 
