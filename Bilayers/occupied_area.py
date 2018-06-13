96411e7d2397155c2a8bcc8445dc144dd0cc8978
ahy3nz <alexander.h.yang@vanderbilt.edu>
2018-06-13 14:46:02 -0500

    Proper logic for looking at maxima of np array elements, returning bins too

--- Bilayers/occupied_area.py
+++ Bilayers/occupied_area.py
@@ -7,17 +7,19 @@ from optparse import OptionParser
 import numpy as np
 from multiprocessing import Pool
 
-def compute_occupied_profile_all(traj lipid_dict, bin_spacing=0.1, z_bins=None, centered=True):
+def compute_occupied_profile_all(traj, bin_spacing=0.1, z_bins=None, centered=True):
     """ Compute void fraction  according to bins
 
     Parameters
     -----------
     traj : mdtraj trajectory
         Single trajectory frame
-    lipid_dict : dict
-        Mapping residue indices to associated atom indices
-    bin_spacing : float
+    bin_spacing : float, opt
         Distance between slices [nm]
+    z_bins : np.array, default None
+        Array of z bins we are evaluating the occupied area at
+    centered : bool, opt,  default True
+        If True, re-center the bilayer
 
     Returns
     -------
@@ -53,7 +55,7 @@ def compute_occupied_profile_all(traj lipid_dict, bin_spacing=0.1, z_bins=None, 
         occupied_profile = pool.starmap(_compute_occupied_profile_frame,
                             zip(itertools.repeat(traj), itertools.repeat(lipid_atoms),
                                 itertools.repeat(z_bins), range(traj.n_frames)))
-    return occupied_profile
+    return z_bins, np.array(occupied_profile)
 
 def _compute_occupied_profile_frame(traj, lipid_atoms, z_bins, frame_i):
     """ Compute bilayer occupied area profile for a particular traj slice
@@ -83,7 +85,7 @@ def _compute_occupied_profile_frame(traj, lipid_atoms, z_bins, frame_i):
     occupied_area_profile = np.zeros_like(z_bins)
     total_area = x_length * y_length
     for i in lipid_atoms:
-        atom_i = topol.atom(i)
+        atom_i = traj.topology.atom(i)
         radius_i = atom_i.element.radius
         atom_i_center = traj.xyz[frame_i, i, :]
 
@@ -93,11 +95,17 @@ def _compute_occupied_profile_frame(traj, lipid_atoms, z_bins, frame_i):
 
         # There may be elements in r_eff_sq that are negative becuase they are so 
         # far away from that z_bin, so add 0 instead
-        occupied_area_profile += max(np.pi * (r_eff_sq), 0)
+        for i, val in enumerate(r_eff_sq):
+            occupied_area_profile[i] += max(np.pi * val, 0)
 
-    f_occ_profile = occupied_area/total_area
+    f_occ_profile = occupied_area_profile/total_area
+    # Don't let occupied fractions exceed 1
+    f_occ_profile = np.array([min(f,1) for f in f_occ_profile])
+    #for i, f in enumerate(f_occ_profile):
+        #f_occ_profile[i] = min(f, 1)
 
-    return frame_i, f_occ_profile
+    return  f_occ_profile
+    #return frame_i, f_occ_profile
 
 
 def _compute_com( traj):
@@ -119,28 +127,19 @@ def _compute_com( traj):
 
 
 if __name__ == "__main__":
-    parser = OptionParser()
-    parser.add_option('-f', action="store", type="string", default = 'nopbc.xtc', dest = 'trajfile')
-    parser.add_option('-c', action="store", type="string", default = 'nopbc.gro', dest = 'grofile')
-    parser.add_option('-o', action="store", type="string", default = 'occupied_area.txt', dest = 'outfile')
-    (options, args) = parser.parse_args()
-
-    outfile = open(options.outfile, 'w')
-
-    print('Loading trajectory <{}>...'.format(options.trajfile))
-    print('Loading topology <{}>...'.format(options.grofile))
-    traj = mdtraj.load(trajfile, top=options.grofile)
-    topol = traj.topology
+    traj = mdtraj.load('npt_500ps.xtc', top='npt_500ps.gro')
 
     # Compute system information
-    print('Gathering system information <{}>...'.format(options.grofile))
-    lipid_dict, headgroup_dict = bilayer_analysis_functions.get_lipids(topol)
-    lipid_tails = bilayer_analysis_functions.get_lipid_tails(topol, lipid_dict)
 
     start = time.time()
-    occupied_area_profile = compute_occupied_profile_all(traj[0], topol, lipid_dict, bin_spacing = 0.1, centered=True)
+    z_bins, occupied_area_profile = compute_occupied_profile_all(traj)
     end = time.time()
-    print("{:30s}: {}".format(options.grofile[:-4], end-start))
-    np.savetxt(outfile, occupied_area_profile)
+    np.savetxt('profile.txt', occupied_area_profile)
+    import matplotlib
+    matplotlib.use('agg')
+    import matplotlib.pyplot as plt
+    plt.plot(occupied_area_profile[-1])
+    plt.savefig('occ.png')
+    plt.close()
 
 
