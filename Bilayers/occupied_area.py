cddfbb3e53b6fff191201f5e08f01e4626a6d005
ahy3nz <alexander.h.yang@vanderbilt.edu>
2018-06-13 15:50:41 -0500

    Perform averaging and statistics on occupied area profile, save, plot

--- Bilayers/occupied_area.py
+++ Bilayers/occupied_area.py
@@ -1,13 +1,17 @@
 from __future__ import print_function
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+
 import mdtraj as mdtraj
 import time
 import bilayer_analysis_functions 
 import itertools
-from optparse import OptionParser
 import numpy as np
 from multiprocessing import Pool
 
-def compute_occupied_profile_all(traj, bin_spacing=0.1, z_bins=None, centered=True):
+def compute_occupied_profile_all(traj, bin_spacing=0.1, z_bins=None, centered=True,
+                                plot=True):
     """ Compute void fraction  according to bins
 
     Parameters
@@ -20,6 +24,8 @@ def compute_occupied_profile_all(traj, bin_spacing=0.1, z_bins=None, centered=Tr
         Array of z bins we are evaluating the occupied area at
     centered : bool, opt,  default True
         If True, re-center the bilayer
+    plot : bool ,opt, default True
+        If True, save the occupied area profile and plot
 
     Returns
     -------
@@ -52,10 +58,29 @@ def compute_occupied_profile_all(traj, bin_spacing=0.1, z_bins=None, centered=Tr
         
     # Need to pass lists of parameters for the mapping function
     with Pool() as pool:
-        occupied_profile = pool.starmap(_compute_occupied_profile_frame,
+        occupied_area_profile = pool.starmap(_compute_occupied_profile_frame,
                             zip(itertools.repeat(traj), itertools.repeat(lipid_atoms),
                                 itertools.repeat(z_bins), range(traj.n_frames)))
-    return z_bins, np.array(occupied_profile)
+    occupied_area_profile = np.array(occupied_area_profile)
+
+    if plot:
+        mean_profile = np.mean(occupied_area_profile, axis=0)
+        std_profile = np.std(occupied_area_profile, axis=0)
+    
+        np.savetxt('occupied_area_profile.dat', np.column_stack((z_bins, 
+                                                mean_profile, std_profile)))
+        fig, ax = plt.subplots(1,1)
+        ax.plot(z_bins, mean_profile)
+        ax.fill_between(z_bins, mean_profile - std_profile, 
+                        mean_profile + std_profile,
+                        alpha=0.4)
+        ax.set_ylabel("Occupied Area Fraction")
+        ax.set_xlabel("Depth (nm)")
+        ax.set_ylim([0,1])
+        fig.savefig('occupied_area_profile.png')
+        plt.close(fig)
+
+    return z_bins, occupied_area_profile
 
 def _compute_occupied_profile_frame(traj, lipid_atoms, z_bins, frame_i):
     """ Compute bilayer occupied area profile for a particular traj slice
@@ -86,26 +111,24 @@ def _compute_occupied_profile_frame(traj, lipid_atoms, z_bins, frame_i):
     total_area = x_length * y_length
     for i in lipid_atoms:
         atom_i = traj.topology.atom(i)
-        radius_i = atom_i.element.radius
-        atom_i_center = traj.xyz[frame_i, i, :]
+        if "H" not in atom_i.element.symbol:
+            radius_i = atom_i.element.radius
+            atom_i_center = traj.xyz[frame_i, i, :]
 
-        # r_eff_sq is a numpy array of the different effective, squared radii
-        # at the different z_bins
-        r_eff_sq = ((radius_i**2) - ((z_bins - atom_i_center[2]) ** 2))
+            # r_eff_sq is a numpy array of the different effective, squared radii
+            # at the different z_bins
+            r_eff_sq = ((radius_i**2) - ((z_bins - atom_i_center[2]) ** 2))
 
-        # There may be elements in r_eff_sq that are negative becuase they are so 
-        # far away from that z_bin, so add 0 instead
-        for i, val in enumerate(r_eff_sq):
-            occupied_area_profile[i] += max(np.pi * val, 0)
+            # There may be elements in r_eff_sq that are negative becuase they are so 
+            # far away from that z_bin, so add 0 instead
+            for i, val in enumerate(r_eff_sq):
+                occupied_area_profile[i] += max(np.pi * val, 0)
 
     f_occ_profile = occupied_area_profile/total_area
     # Don't let occupied fractions exceed 1
     f_occ_profile = np.array([min(f,1) for f in f_occ_profile])
-    #for i, f in enumerate(f_occ_profile):
-        #f_occ_profile[i] = min(f, 1)
 
     return  f_occ_profile
-    #return frame_i, f_occ_profile
 
 
 def _compute_com( traj):
@@ -127,19 +150,13 @@ def _compute_com( traj):
 
 
 if __name__ == "__main__":
-    traj = mdtraj.load('npt_500ps.xtc', top='npt_500ps.gro')
+    traj = mdtraj.load('npt_80-100ns.xtc', top='npt.gro')
 
-    # Compute system information
 
     start = time.time()
     z_bins, occupied_area_profile = compute_occupied_profile_all(traj)
     end = time.time()
-    np.savetxt('profile.txt', occupied_area_profile)
-    import matplotlib
-    matplotlib.use('agg')
-    import matplotlib.pyplot as plt
-    plt.plot(occupied_area_profile[-1])
-    plt.savefig('occ.png')
-    plt.close()
+    #print('Occ profile took: {}'.format(end-start))
+
 
 
