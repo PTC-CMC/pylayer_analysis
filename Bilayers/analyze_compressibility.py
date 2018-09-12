068311540286df38918e9bd4226bc1ef23301869
Alex Yang <yangah@whitby.vuse.vanderbilt.edu>
2018-09-12 13:45:36 -0500

    Use chunking for analyzing long trajectories compressibilities

--- Bilayers/analyze_compressibility.py
+++ Bilayers/analyze_compressibility.py
@@ -6,13 +6,23 @@ import bilayer_analysis_functions
 import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
+
 import plot_ay
 plot_ay.setDefaults()
 
 kb = u.BOLTZMANN_CONSTANT_kB 
+#kb = u.BOLTZMANN_CONSTANT_kB * u.AVOGADRO_CONSTANT_NA
 T = 305 * u.kelvin
-traj = mdtraj.load('trajectory.dcd', top='npt.gro')
-areas = traj.unitcell_lengths[:,0] * traj.unitcell_lengths[:,1] * u.nanometer**2
+#traj = mdtraj.load('trajectory.dcd', top='npt.gro')
+areas = []
+n_frames = 0
+for chunk in mdtraj.iterload('trajectory.dcd', top='npt.gro'):
+    area_chunk = chunk.unitcell_lengths[:,0] * chunk.unitcell_lengths[:,1]
+    areas = np.concatenate((areas, area_chunk))
+    n_frames += chunk.n_frames
+areas *= u.nanometer**2
+#areas = traj.unitcell_lengths[:,0] * traj.unitcell_lengths[:,1] * u.nanometer**2
+#areas, _= bilayer_analysis_functions.block_avg(traj, areas._value)*areas.unit
 avg_area = np.mean(areas)
 mean_sq_fluc = np.sum((areas - avg_area)**2)/len(areas)
 ka = kb*T*avg_area/mean_sq_fluc
@@ -20,14 +30,17 @@ print(avg_area/64)
 print(ka.in_units_of(u.dyne/u.centimeter))
 print(ka.in_units_of(u.newton/u.meter))
 
+#for i, frame in enumerate(traj):
+#    frame.time = 10*i
+
 # we have 10,000 frames for 100ns
 # we now have 50,000 frames for 500ns
-# Let's declare block size as a function of frames
+# Let's declare block size
 block_sizes = [100,200,300,400, 500,600,700,800,900, 1000, 2000, 5000,
             10000, 20000, 25000]
 fig, ax = plt.subplots(1,2)
 for block_size in block_sizes:
-    n_blocks = int(traj.n_frames / block_size)
+    n_blocks = int(n_frames / block_size)
     all_ka = []
     for i in range(n_blocks):
         window_areas = areas[i : i + block_size]
@@ -36,7 +49,8 @@ for block_size in block_sizes:
         ka = kb*T*window_avg/window_fluc
         all_ka.append(ka.in_units_of(u.dyne/u.centimeter)._value)
     mean = np.mean(all_ka)
-    se = np.std(all_ka)/np.sqrt(n_blocks)
+    #se = np.std(all_ka)/np.sqrt(n_blocks)
+    se = np.std(all_ka)
     ax[0].scatter([block_size], [se])
     ax[1].scatter([block_size], [mean])
     print("{} blocksize, {} mean, {} se".format(block_size, mean, se))
@@ -53,7 +67,7 @@ plt.close(fig)
 # 50000 frames for 500ns
 corr_length = 25000
 dt = 10
-time_origins = np.arange(0, traj.n_frames - corr_length, step=dt)
+time_origins = np.arange(0, n_frames - corr_length, step=dt)
 all_corrs = []
 for origin in time_origins:
     avg_area = np.mean(areas[origin:origin+corr_length])
@@ -68,11 +82,10 @@ fig.tight_layout()
 fig.savefig('correlations.png')
 plt.close(fig)
 
-# Look at areas
 fig, ax = plt.subplots(1,1)
-ax.plot(np.arange(0, len(areas)), areas._value)
-ax.set_xlabel("Frame")
-ax.set_ylabel("Area")
+ax.plot(np.arange(0, len(areas)), areas)
+ax.set_xlabel("Frames")
+ax.set_ylabel("Box Area")
 ax.grid()
 fig.tight_layout()
 fig.savefig('areas.png')
